d3a558ffae2830c289c143f02c4f0d6e5044d0a5
Kristi Nikolla <knikolla@bu.edu>
2026-09-01 17:03:59 -0400

    Add CILogon Identity Provider
    - Initial file was auto-generated but some fields were not
      populated properly and some others still want to be
      reapplied despite matching the server state.
    - Verify essential claims, which we are using for domain
      filtering during registration seems to not be something
      exposed by the terraform configuration directly, but the
      provider says those extra values can be placed in the
      `extra_config` object.

--- data.tf
+++ data.tf
@@ -2,6 +2,11 @@ data "aws_secretsmanager_secret_version" "smtp_credentials" {
   secret_id = "cluster/moc-services/keycloak/smtp"
 }
 
+data "aws_secretsmanager_secret_version" "cilogon_credentials" {
+  secret_id = "cluster/moc-services/keycloak/cilogon"
+}
+
 locals {
-  smtp_credentials = jsondecode(data.aws_secretsmanager_secret_version.smtp_credentials.secret_string)
+  smtp_credentials    = jsondecode(data.aws_secretsmanager_secret_version.smtp_credentials.secret_string)
+  cilogon_credentials = jsondecode(data.aws_secretsmanager_secret_version.cilogon_credentials.secret_string)
 }
---
+++ idp_cilogon.tf
@@ -0,0 +1,28 @@
+resource "keycloak_oidc_identity_provider" "cilogon" {
+  alias                    = "cilogon"
+  authenticate_by_default  = false
+  authorization_url        = "https://cilogon.org/authorize"
+  backchannel_supported    = false
+  client_id                = local.cilogon_credentials.client_id
+  client_secret_wo         = local.cilogon_credentials.client_secret
+  client_secret_wo_version = 1
+  default_scopes           = "openid email profile org.cilogon.userinfo"
+  display_name             = "cilogon"
+  enabled                  = true
+  extra_config = {
+    "filteredByClaim"  = "true"
+    "claimFilterName"  = "email"
+    "claimFilterValue" = "(?i)^[^@\\s]+@(bu\\.edu|redhat\\.com)$"
+  }
+  first_broker_login_flow_alias = "CILogon First Broker Login"
+  issuer                        = "https://cilogon.org"
+  jwks_url                      = "https://cilogon.org/oauth2/certs"
+  login_hint                    = "false"
+  logout_url                    = ""
+  realm                         = "moc"
+  store_token                   = false
+  sync_mode                     = "IMPORT"
+  token_url                     = "https://cilogon.org/oauth2/token"
+  trust_email                   = false
+  user_info_url                 = "https://cilogon.org/oauth2/userinfo"
+}

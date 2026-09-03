c50494b1ab25ce6eb9127902c81f9769af739479
Lars Kellogg-Stedman <lars@redhat.com>
2026-09-03 07:32:25 -0400

    Move imports from main.tf to imports.tf
    Our local testing logic expects all imports to live in `imports.tf`. With
    imports in `main.tf` it's not possible to apply this repository against a
    fresh keycloak instance because the referenced resources will not exist.

--- imports.tf
+++ imports.tf
@@ -1,4 +1,3 @@
-
 import {
   to = keycloak_realm.moc
   id = "moc"
@@ -7,4 +6,22 @@ import {
 import {
   to = keycloak_oidc_identity_provider.cilogon
   id = "moc/cilogon"
+}
+
+import {
+  for_each = local.openshift_oidc_clusters
+  to       = module.openshift_oidc[each.key].keycloak_openid_client.this
+  id       = "${local.realm_id}/${each.value.keycloak_client_uuid}"
+}
+
+import {
+  for_each = local.openshift_oidc_clusters
+  to       = module.openshift_oidc[each.key].aws_secretsmanager_secret.this
+  id       = each.value.client_secret_name # Or each.value.secret_arn
+}
+
+import {
+  for_each = local.openshift_oidc_clusters
+  to       = module.openshift_oidc[each.key].aws_secretsmanager_secret_version.this
+  id       = "${each.value.secret_arn}|${each.value.secret_version_id}"
 }

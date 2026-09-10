3f371e29732c0ba9eb9f24b41b7ce935d6ab2162
Kristi Nikolla <knikolla@bu.edu>
2026-09-10 09:21:35 -0400

    Update first broker login flow alias in CILogon config

--- idp_cilogon.tf
+++ idp_cilogon.tf
@@ -9,7 +9,7 @@ resource "keycloak_oidc_identity_provider" "cilogon" {
   default_scopes                = "openid email profile org.cilogon.userinfo"
   display_name                  = "cilogon"
   enabled                       = true
-  first_broker_login_flow_alias = keycloak_authentication_flow.cilogon_first_broker_login.alias
+  first_broker_login_flow_alias = "Require Existing User First Broker Login"
   issuer                        = "https://cilogon.org"
   jwks_url                      = "https://cilogon.org/oauth2/certs"
   login_hint                    = "false"
